6b2cdb3f63b5b4d9afb6bf7862fc643713c44f93
rodPinheiro <rod_bma@hotmail.com>
2022-02-15 03:56:22 -0300

    1

--- script.sh
+++ script.sh
@@ -4,10 +4,15 @@ echo 'insira a url ssh do repositorio de destino '
 
 read REPO1
 
+echo 'insira o nome exato da sua branch'
+
+read BRANCH1
+
 git remote add temp $REPO1
 
-git push -u temp $REPO1
+
+git push temp $BRANCH1:main
 
 git remote remove temp 
 
-echo 'repositorio copiado!'
+echo 'Processo terminado'
