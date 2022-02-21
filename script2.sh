f66809c1d6c51a2975f13ef002df73af385cd42c
rodPinheiro <rod_bma@hotmail.com>
2022-02-21 17:58:13 -0300

    Implementation of tests and README deleting

--- script2.sh
+++ script2.sh
@@ -1,4 +1,4 @@
-# Esse script depende que pacote gh do github esteja instalado 
+# Esse script depende que pacote gh do github esteja instalado
 # cria a repositorio remoto e faz o push automaticamente
 
 # execucao do script: ./script2.sh myproject projectFolder
@@ -14,18 +14,32 @@ read USER_BRANCH_INPUT
 USER_BRANCH=$(git --git-dir $2/.git for-each-ref --format="%(if)%(authorname)%(then) %(refname:short)%(end)" refs/heads/"*$USER_BRANCH_INPUT*" )
 
 
+
+
+
 git --git-dir $2/.git checkout $USER_BRANCH
 git --git-dir $2/.git checkout -b main
 
-gh repo create $1 --private --source=$2 --remote=temp --push
-
-
+if [ "$3" = "private" ]
+then
+    gh repo create $1 --private --source=$2 --remote=temp --push
+else
+    read -p "Voce gostaria de apagar os testes e o README?
+    Isso apagara todos os .test.js do projeto (y/n)?" CONT
+    if [ $CONT = "y" ]
+    then
+        git --git-dir $2/.git --work-tree=$2 rm './*.test.js' './*.md' -r
+        git --git-dir $2/.git --work-tree=$2 commit -a -m "Remove os testes e o README da Trybe"
+        
+    fi
+    gh repo create $1 --public --source=$2 --remote=temp --push
+fi
 
 git --git-dir $2/.git checkout $USER_BRANCH
 git --git-dir $2/.git branch -D main
 
 
-git --git-dir $2/.git remote remove temp 
+git --git-dir $2/.git remote remove temp
 
 gh repo view $1 -w
 
